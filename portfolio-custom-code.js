623d1fea209fa121ff0da9657303afc8997a7294
Maciej Sawicki <saw.maciej@gmail.com>
2017-09-16 18:09:50 +0200

    no message

--- portfolio-custom-code.js
+++ portfolio-custom-code.js
@@ -1,3 +1,4 @@
+/* Filtering elements */
 $(document).on('click', '[action-toggle-filter]', function() {
 	var filterToToggle = $(this).attr('action-toggle-filter');
 	QueryStringRouter.setParam('filter', filterToToggle);
@@ -24,3 +25,59 @@ QueryStringRouter.onParamChange('filter', function(value) {
 	}
 });
 
+
+//on scroll effects and playing videos when not in scroll view
+$(document).ready(function() {
+    var media = $('video');
+    var tolerancePixel = 80;
+    var elementsWithIsometricEffect =  $('[has-isometric-effect]');
+    
+    //pause all videos except first
+    media.each(function(index, el) {
+        if (index > 0) {
+        	  $(this).get(0).pause();
+        } else {
+            $(this).get(0).play();
+        }
+    });
+    
+    //add playsinline attribute for all vidoes
+    media.each(function(index, el) {
+        $(this).attr('playsinline', '');
+    });
+    
+    //fix webflow placement of video poster
+    $('.screencast-video .w-background-video-poster').css('background-position', '50% 0%');
+
+    function checkOnScrollEffects() {
+        var scrollTop = $(window).scrollTop() + tolerancePixel;
+        var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+        media.each(function(index, el) {
+            var yTopMedia = $(this).offset().top;
+            var yBottomMedia = $(this).height() + yTopMedia;
+
+            if (scrollTop < yBottomMedia && scrollBottom > yTopMedia) {
+                $(this).get(0).play();
+                $(this).closest('.screencast-video').removeClass('is-hidden');
+            } else {
+                $(this).get(0).pause();
+                $(this).closest('.screencast-video').addClass('is-hidden');
+            }
+        });
+        
+        elementsWithIsometricEffect.each(function(index, el) {
+            var yTop = $(this).offset().top;
+            var yBottom = $(this).height() + yTop;
+
+            if (scrollTop < yBottom && scrollBottom > yTop) {
+                $(this).removeClass('is-3d-isometric');
+            } else {
+                $(this).addClass('is-3d-isometric');
+            }
+        });
+
+    }
+    
+    $(document).on('scroll', checkOnScrollEffects);
+});
